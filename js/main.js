09024e8161720b130869963f0fb86338275644cc
zitongliu <zitongliu7@gmail.com>
2017-02-02 10:50:07 +1100

    Added jQuery and Typed.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+$(function(){
+    $("#intro-text").typed({
+        strings: ["Hello, my name is Steve ...", "I am a full stack web developer.","I want to build "],
+        // typing speed
+        typeSpeed: 30,
+        // time before typing starts
+        startDelay: 0,
+        // backspacing speed
+        backSpeed: 10,
+        // time before backspacing
+        backDelay: 800,
+        // loop
+        loop: true,
+        // false = infinite
+        loopCount: false,
+        
+    });
+});
